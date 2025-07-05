fa4d3c9488222c6b37ea5f3e0d382b6ed457dcde
Emeric <caramanna.emeric@gmail.com>
2025-07-05 11:50:26 +0200

    Disabled arrow keys to force me to use vim motions

--- after/plugin/rust-tools.lua
+++ after/plugin/rust-tools.lua
@@ -4,9 +4,9 @@ local rt = require("rust-tools")
 rt.setup({
   server = {
     on_attach = function(_, bufnr)
-      -- Hover actions
+      -- Show contextual documentation/type info under cursor (fancy popup)
       vim.keymap.set("n", "<Leader><Leader>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
+      -- Show grouped code actions (e.g. fixes, refactors), no selection needed
       vim.keymap.set("n", "<Leader>c", rt.code_action_group.code_action_group, { buffer = bufnr })
     end,
   },
--- lua/codesage/remap.lua
+++ lua/codesage/remap.lua
@@ -1,3 +1,18 @@
 vim.g.mapleader = " "
 -- While in normal mode (n) if I press <leader>pv it will run Ex (file explorer)
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Disable arrow keys to force me to use vim motions
+vim.keymap.set("n", "<Up>", ":echoe 'Get off my lawn!'<CR>")
+vim.keymap.set("n", "<Down>", ":echoe 'Get off my lawn!'<CR>")
+vim.keymap.set("n", "<Left>", ":echoe 'Get off my lawn!'<CR>")
+vim.keymap.set("n", "<Right>", ":echoe 'Get off my lawn!'<CR>")
+vim.keymap.set("i", "<Up>", "<C-o>:echoe 'Get off my lawn!'<CR>")
+vim.keymap.set("i", "<Down>", "<C-o>:echoe 'Get off my lawn!'<CR>")
+vim.keymap.set("i", "<Left>", "<C-o>:echoe 'Get off my lawn!'<CR>")
+vim.keymap.set("i", "<Right>", "<C-o>:echoe 'Get off my lawn!'<CR>")
+vim.keymap.set("v", "<Up>", ":<C-u>echoe 'Get off my lawn!'<CR>")
+vim.keymap.set("v", "<Down>", ":<C-u>echoe 'Get off my lawn!'<CR>")
+vim.keymap.set("v", "<Left>", ":<C-u>echoe 'Get off my lawn!'<CR>")
+vim.keymap.set("v", "<Right>", ":<C-u>echoe 'Get off my lawn!'<CR>")
+
